ad556fb0f7c6d6081b0b0580e826c389aaa1cf4b
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-24 20:29:31 -0700

    mobile compatibility

--- source/funkin/backend/shaders/OverlayShader.hx
+++ source/funkin/backend/shaders/OverlayShader.hx
@@ -6,27 +6,30 @@ import flixel.system.FlxAssets.FlxShader;
 class OverlayShader extends FlxShader
 {
 	@:glFragmentSource('
-#pragma header
-uniform vec4 uBlendColor;
+	#pragma header
 
-vec3 blendLighten(base:Vec3, blend:Vec3) : Vec3 {
-	return mix(
-		1.0 - 2.0 * (1.0 - base) * (1.0 - blend),
-		2.0 * base * blend,
-		step( base, vec3(0.5) )
-	);
-}
+	uniform vec4 uBlendColor;
 
-vec4 blendLighten(vec4 base, vec4 blend, float opacity)
-{
-	return (blendLighten(base, blend) * opacity + base * (1.0 - opacity));
-}
+	vec3 overlay(vec3 base, vec3 blend)
+	{
+		return mix(
+			2.0 * base * blend,
+			1.0 - 2.0 * (1.0 - base) * (1.0 - blend),
+			step(0.5, base)
+		);
+	}
 
-void main()
-{
-	vec4 base = texture2D(bitmap, openfl_TextureCoordv);
-	gl_FragColor = blendLighten(base, uBlendColor, uBlendColor.a);
-}')
+	void main()
+	{
+		vec4 tex = flixel_texture2D(bitmap, openfl_TextureCoordv);
+
+		vec3 color = overlay(tex.rgb, uBlendColor.rgb);
+
+		color = mix(tex.rgb, color, uBlendColor.a);
+
+		gl_FragColor = vec4(color, tex.a);
+	}
+	')
 	public function new()
 	{
 		super();
