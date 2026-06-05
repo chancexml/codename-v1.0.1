251b849d51414b0d86eea9fc7ff0e24fb200d34d
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-06-04 18:25:05 -0700

    Enhance pixel script for mobile support
    Refactor pixel handling and UI elements for mobile compatibility.

--- assets/data/scripts/pixel.hx
+++ assets/data/scripts/pixel.hx
@@ -1,176 +1,135 @@
-//
-import funkin.game.HudCamera;
-import funkin.backend.scripting.events.NoteHitEvent;
+import funkin.ui.FunkinText;
+import flixel.text.FlxText;
+import flixel.text.FlxTextBorderStyle;
+import flixel.util.FlxAxes;
+#if mobile
+import mobile.controls.VirtualPad;
+import mobile.controls.FlxButton;
+#end
 
-public var pixelNotesForBF = true;
-public var pixelNotesForDad = true;
-public var pixelSplashes = true;
-public var enablePixelUI = true;
-public var enablePixelGameOver = true;
-public var enableCameraHacks = Options.week6PixelPerfect;
-public var enablePauseMenu = true;
-public var isSpooky = false;
+#if mobile
+var virtualPad:VirtualPad;
+#end
 
-var oldStageQuality = FlxG.game.stage.quality;
-public var daPixelZoom = PlayState.daPixelZoom;
+var pixelScript:Script = game.scripts.getByName("pixel.hx");
+var pauseCam = new FlxCamera();
 
-/**
- * UI
- */
-function onNoteCreation(event) {
-	if ((event.note.strumLine == playerStrums && !pixelNotesForBF) || (event.note.strumLine == cpuStrums && !pixelNotesForDad)) return;
+var bg:FlxSprite;
+var hand:FlxSprite;
+
+var texts:Array<FlxText> = [];
+
+var isThorns = PlayState.SONG.meta.name.toLowerCase() == "thorns";
+
+function create(event) {
+	// cancel default pause menu!!
 	event.cancel();
 
-	var note = event.note;
-	var strumID = event.strumID;
-	if (event.note.isSustainNote) {
-		note.loadGraphic(Paths.image('stages/school/ui/arrowEnds'), true, 7, 6);
-		var maxCol = Math.floor(note.graphic.width / 7);
-		note.animation.add("hold", [strumID%maxCol]);
-		note.animation.add("holdend", [maxCol + strumID%maxCol]);
-	} else {
-		note.loadGraphic(Paths.image('stages/school/ui/arrows-pixels'), true, 17, 17);
-		var maxCol = Math.floor(note.graphic.width / 17);
-		note.animation.add("scroll", [maxCol + strumID%maxCol]);
-	}
-	var strumScale = event.note.strumLine.strumScale;
-	note.scale.set(daPixelZoom*strumScale, daPixelZoom*strumScale);
-	note.updateHitbox();
-	note.antialiasing = false;
-}
+	event.music = isThorns ? "pixel/LunchboxScary" : "pixel/breakfast";
 
-function onPostNoteCreation(event) if (pixelSplashes)
-	event.note.splash = "pixel-default";
+	// allowing this pause script even if the pixel script is not loaded  - Nex
+	pixelScript?.call("pixelCam", [pauseCam]);
 
-function onStrumCreation(event) {
-	if ((event.player == 1 && !pixelNotesForBF) || (event.player == 0 && !pixelNotesForDad)) return;
-	event.cancel();
+	FlxG.cameras.add(pauseCam, false);
+	cameras = [pauseCam];
 
-	var strum = event.strum;
-	strum.loadGraphic(Paths.image('stages/school/ui/arrows-pixels'), true, 17, 17);
-	var maxCol = Math.floor(strum.graphic.width / 17);
-	var strumID = event.strumID % maxCol;
+	pauseCam.bgColor = isThorns ? 0x88000000 : 0x88FF99CC;
+	pauseCam.alpha = 0;
 
-	strum.animation.add("static", [strumID]);
-	strum.animation.add("pressed", [maxCol + strumID, (maxCol*2) + strumID], 12, false);
-	strum.animation.add("confirm", [(maxCol*3) + strumID, (maxCol*4) + strumID], 24, false);
+	bg = new FlxSprite(44 * 6, 14 * 6, Paths.image('stages/school/pause/bg'));
+	bg.antialiasing = false;
+	if (isThorns) bg.color = 0xFF000000;
+	bg.scale.set(6, 6);
+	bg.updateHitbox();
+	bg.scale.y = 4;
+	add(bg);
 
-	var strumScale = strumLines.members[event.player].strumScale;
-	strum.scale.set(daPixelZoom*strumScale, daPixelZoom*strumScale);
-	strum.updateHitbox();
-	strum.antialiasing = false;
-}
+	songText = new FlxText(0, 22 * 6, 0, "Pause", 8, false);
+	confText(songText);
+	add(songText);
 
-function onCountdown(event) {
-	if (!enablePixelUI) return;
-
-	if (event.soundPath != null) event.soundPath = 'pixel/' + event.soundPath;
-	event.antialiasing = false;
-	event.scale = daPixelZoom;
-	event.spritePath = switch(event.swagCounter) {
-		case 0: null;
-		case 1: 'stages/school/ui/ready';
-		case 2: 'stages/school/ui/set';
-		case 3: 'stages/school/ui/go';
-	};
-}
-
-function onPlayerHit(event:NoteHitEvent) {
-	if (!enablePixelUI) return;
-	event.ratingPrefix = "stages/school/ui/";
-	event.ratingScale = daPixelZoom * 0.7;
-	event.ratingAntialiasing = false;
-
-	event.numScale = daPixelZoom;
-	event.numAntialiasing = false;
-}
-
-/**
- * CAMERA HACKS!!
- */
-function postCreate() {
-	if (enablePauseMenu)
-		PauseSubState.script = 'data/scripts/week6-pause';
-
-	if (enableCameraHacks) {
-		camGame.pixelPerfectRender = true;
-		camGame.antialiasing = false;
-
-		makeCameraPixely(camGame);
-		defaultCamZoom /= daPixelZoom;
+	for (i=>e in menuItems) {
+		text = new FlxText(0, (22 * 6) + ((i+2) * 9 * 6), 0, e, 8, false);
+		confText(text);
 	}
 
-	if (enablePixelGameOver) {
-		gameOverSong = "pixel/gameOver";
-		lossSFX = "pixel/gameOverSFX";
-		retrySFX = "pixel/gameOverEnd";
-	}
+	hand = new FlxSprite().loadGraphic(Paths.image('stages/school/ui/hand_textbox'));
+	hand.antialiasing = false;
+	hand.scale.set(6, 6);
+	hand.updateHitbox();
+	add(hand);
+
+	FlxTween.tween(bg.scale, {y: 6}, 0.75, {ease: FlxEase.elasticOut});
+
+
+	FlxG.sound.play(Paths.sound(isThorns ? 'pixel/ANGRY' : 'pixel/clickText'));
+
+	#if mobile
+	addVirtualPad('UP_DOWN', 'A_B');
+	#end
 }
 
-/*function onStartCountdown() {
-	var newNoteCamera = new HudCamera();
-	newNoteCamera.bgColor = 0; // transparent
-	FlxG.cameras.add(newNoteCamera, false);
+function confText(text) {
+	text.scale.set(6, 6);
+	text.updateHitbox();
+	text.screenCenter(FlxAxes.X);
+	text.borderStyle = FlxTextBorderStyle.OUTLINE;
+	if (!isThorns) text.borderColor = 0xFF953E3E;
 
-	var pixelSwagWidth = Note.swagWidth + (daPixelZoom - (Note.swagWidth % daPixelZoom));
-
-	for(p in strumLines) {
-		var i = 0;
-		for(str in p.members) {
-			str.x = (FlxG.width * strumOffset) + (pixelSwagWidth * (i - 2));
-			str.x -= str.x % daPixelZoom;
-			i++;
-		}
-	}
-	makeCameraPixely(newNoteCamera);
-}*/
-
-/**
- * Use this to make any camera pixelly (you wont be able to zoom with it anymore!)
- */
-public function makeCameraPixely(cam) {
-	cam.pixelPerfectRender = true;
-	if(!enableCameraHacks) return;
-
-	cam.zoom /= Math.min(FlxG.scaleMode.scale.x, FlxG.scaleMode.scale.y) * daPixelZoom;
-
-	var shad = new CustomShader('pixelZoomShader');
-	cam.addShader(shad);
-
-	pixellyCameras.push(cam);
-	pixellyShaders.push(shad);
-
-	FlxG.game.stage.quality = 2;
+	text.antialiasing = false;
+	texts.push(text);
+	add(text);
 }
 
-function destroy() {
-	// resets the stage quality
-	FlxG.game.stage.quality = oldStageQuality;
+function destroy() 
+if (FlxG.cameras.list.contains(pauseCam))
+	FlxG.cameras.remove(pauseCam);
+
+var canDoShit = true;
+var time:Float = 0;
+function update(elapsed) {
+	#if mobile
+    if (virtualPad != null) virtualPad.update(elapsed);
+	#end
+	pixelScript?.call("postUpdate", [elapsed]);
+
+	pauseCam.alpha = lerp(pauseCam.alpha, 1, 0.25);
+	time += elapsed;
+
+	var curText = texts[curSelected + 1];
+	hand.setPosition(curText.x - hand.width - 18 + (Math.sin(time * Math.PI * 2) * 12), curText.y + (text.height - hand.height) - 6);
+	hand.x -= hand.x % 6;
+	hand.y -= hand.y % 6;
+
+	changeSelection((controls.UP_P ? -1 : 0) + (controls.DOWN_P ? 1 : 0) - FlxG.mouse.wheel);
+
+	if (controls.ACCEPT) enterOption();
 }
 
-function pixelCam(cam)
-	makeCameraPixely(cam);
+var scrollSFX = FlxG.sound.load(Paths.sound(isThorns ? 'pixel/type' : 'pixel/pixelText'));
+function changeSelection(change) if (canDoShit) {  // this overrides the function inside of the normal pause btw, so no event gets called  - Nex
+	curSelected = FlxMath.wrap(curSelected + change, 0, menuItems.length - 1);
+	if (change != 0) scrollSFX.play();
+}
 
-var pixellyCameras = [];
-var pixellyShaders = [];
+var enterSFX = FlxG.sound.load(Paths.sound(isThorns ? 'pixel/ANGRY' : 'pixel/clickText'));
+function enterOption() if (canDoShit) {
+	var option = menuItems[curSelected];
+	enterSFX.play();
 
-function postUpdate() {
-	for (e in pixellyCameras) if (Std.isOfType(e, HudCamera))
-		e.downscroll = camHUD.downscroll;
-
-	if (enableCameraHacks) for (p in strumLines) {
-		p.notes.forEach(function(n) {
-			if(n.isSustainNote) return; // hacky fix for hold
-			n.y -= n.y % daPixelZoom;
-			n.x -= n.x % daPixelZoom;
-		});
+	switch(option) {
+		case "Resume", "Exit to menu":
+			canDoShit = false;
+			#if mobile
+			if (virtualPad != null) {
+		        remove(virtualPad);
+		        virtualPad.destroy();
+	        	virtualPad = null;
+        	}
+            #end
+			for(t in texts) t.visible = false;
+			hand.visible = songText.visible = false;
+			FlxTween.tween(bg.scale, {y: 0}, 0.125, {ease: FlxEase.cubeOut, onComplete: selectOption});
+		default: selectOption();
 	}
-
-	var zoom = 1 / daPixelZoom / Math.min(FlxG.scaleMode.scale.x, FlxG.scaleMode.scale.y);
-	for (e in pixellyCameras) {
-		if (!e.exists) continue;
-		e.zoom = zoom;
-	}
-	for (e in pixellyShaders)
-		e.pixelZoom = zoom;
 }
